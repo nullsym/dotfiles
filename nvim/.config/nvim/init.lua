70a25e95a367d7e121ee6acd44135a4a941e2464
nullsym <1633888+nullsym@users.noreply.github.com>
2025-08-02 14:37:52 -0400

    Move colored column settings from init.lua to snacks.lua
    Make it a toggle

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,3 @@
 require("config.lazy")
 require("config.man")
 vim.cmd.colorscheme("tokyonight-night")
--- Highlight column
-vim.opt.colorcolumn = "100"
-vim.cmd([[highlight ColorColumn guibg=#202020]])
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,12 +1,44 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          layout = { layout = { position = "right" } },
+  lazy = false,
+  config = function()
+    local Snacks = require("snacks")
+    -- Have to run setup explicitly because calling config overwrote
+    -- the default setup and thus we ended with no notifier, dashboard, etc.
+    Snacks.setup({
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      toggle = { map = LazyVim.safe_keymap_set },
+      words = { enabled = true },
+      dashboard = { enabled = true },
+      -- Move snack explorer to the right
+      picker = {
+        sources = {
+          explorer = {
+            layout = { layout = { position = "right" } },
+          },
         },
       },
-    },
-  },
+    })
+    -- Toggle colored vertical line at column 100
+    Snacks.toggle({
+      name = "Color Column",
+      get = function()
+        return vim.o.colorcolumn == "100"
+      end,
+      set = function(state)
+        if state then
+          vim.o.colorcolumn = "100"
+          vim.cmd([[highlight ColorColumn guibg=#202020]])
+        else
+          vim.o.colorcolumn = ""
+          vim.cmd([[highlight ColorColumn guibg=None]])
+        end
+      end,
+    }):map("<leader>ut", { desc = "Toggle Color Column" })
+  end,
 }
